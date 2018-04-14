8fcec469598234a7e964e96cd721dc9f75d27f68
Ivson Santos <santosivson@hotmail.com>
2018-04-14 19:22:47 -0300

    Criado a pagina com o login padrao

---
+++ src/main/java/com/ivson/security/SecurityAula02Application.java
@@ -0,0 +1,12 @@
+package com.ivson.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityAula02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityAula02Application.class, args);
+	}
+}
---
+++ src/test/java/com/ivson/security/SecurityAula02ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ivson.security;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SecurityAula02ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
