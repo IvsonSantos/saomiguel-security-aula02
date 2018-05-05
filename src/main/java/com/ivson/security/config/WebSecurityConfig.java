5b664680d5502f216727867bf54b73747f5375d3
Ivson Santos <santosivson@hotmail.com>
2018-05-05 19:29:25 -0300

    Projeto atualizado

--- src/main/java/com/ivson/security/config/WebSecurityConfig.java
+++ src/main/java/com/ivson/security/config/WebSecurityConfig.java
@@ -15,27 +15,28 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
-            .authorizeRequests()
-                .antMatchers("/", "/home").permitAll()
-                .anyRequest().authenticated()
-                .and()
-            .formLogin()
-                .loginPage("/login")
-                .permitAll()
-                .and()
-            .logout()
-                .permitAll();
+        
+    	http.authorizeRequests()
+            	.antMatchers("/", "/home").permitAll()
+            	.anyRequest().authenticated()
+            .and()
+            	.formLogin()
+            	.loginPage("/login")
+            	.permitAll()
+            .and()
+            	.logout()
+            	.permitAll();
     }
 
     @Bean
     @Override
     public UserDetailsService userDetailsService() {
     	
-        UserDetails user =
+        @SuppressWarnings("deprecation")
+		UserDetails user =
              User.withDefaultPasswordEncoder()
-                .username("user")
-                .password("password")
+                .username("usuario")
+                .password("senha")
                 .roles("USER")
                 .build();
 
